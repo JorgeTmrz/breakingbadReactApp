3d19450322ab123c729b31c8857f56bfcbca5590
Jorge Tamariz <jstamariz31@gmail.com>
2020-07-06 02:40:11 -0400

    Production Build

---
+++ build/precache-manifest.2d4f63e384a54ba4ce37a1f76e223c7e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9770d33c3982268170e718cdf2f9c944",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c4b74255de7c7c48866",
+    "url": "/static/css/main.aceea456.chunk.css"
+  },
+  {
+    "revision": "b14261e6cb9ed34c1893",
+    "url": "/static/js/2.a659b1fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a659b1fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c4b74255de7c7c48866",
+    "url": "/static/js/main.a9cabe55.chunk.js"
+  },
+  {
+    "revision": "fb746c9ad1d6578fa0ab",
+    "url": "/static/js/runtime-main.4ce1a1e1.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/static/media/spinner.235b9b3e.gif"
+  }
+]);
